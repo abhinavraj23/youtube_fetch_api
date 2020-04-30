d620cdf1ea0b90054586b4dc3a12b344e5e4274f
abhinavraj23 <abhinav.23.april@gmail.com>
2020-05-01 02:55:34 +0530

    Made model for db

--- youtube_fetch_api/api/models.py
+++ youtube_fetch_api/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Videos(models.Model):
+    video_id = models.CharField(
+        null= False,
+        blank= False,
+        max_length= 200
+    )
+
+    title = models.CharField(
+        null= True,
+        blank= True,
+        max_length= 500
+    )
+
+    description = models.CharField(
+        null= True,
+        blank= True,
+        max_length= 5000
+    )
+
+    publishedDateTime = models.DateTimeField()
+
+    thumbnailsUrls = models.URLField()
+
+    channel_id =  models.CharField(
+        null= False,
+        blank= False,
+        max_length= 500
+    )
+
+    channel_title = models.CharField(
+        null= True,
+        blank= True,
+        max_length= 500
+    )
