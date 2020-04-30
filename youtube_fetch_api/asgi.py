112ca4e4eb2c55ee990b051613ceee8ecbb234ed
abhinavraj23 <abhinav.23.april@gmail.com>
2020-05-01 01:24:14 +0530

    Changed project structure

--- youtube_fetch_api/asgi.py
+++ youtube_fetch_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for api project.
+ASGI config for youtube_fetch_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_fetch_api.settings')
 
 application = get_asgi_application()
--- youtube_fetch_api/urls.py
+++ youtube_fetch_api/urls.py
@@ -1,4 +1,4 @@
-"""api URL Configuration
+"""youtube_fetch_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
